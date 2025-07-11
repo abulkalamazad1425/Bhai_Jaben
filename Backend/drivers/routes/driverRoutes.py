5c1127340bacf7e22989f2ae6164e2ebdfb8b7ba
abulkalamazad1425 <bsse1425@iit.du.ac.bd>
2025-07-11 12:26:33 +0600

    router for updating profile

--- Backend/drivers/routes/driverRoutes.py
+++ Backend/drivers/routes/driverRoutes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from ..services.driverServices import DriverService
-from ..schemas.driverSchemas import DriverProfileOut 
+from ..schemas.driverSchemas import DriverProfileOut, DriverProfileUpdate 
 driver_router = APIRouter(prefix="/driver", tags=["Driver"])
 driver_service = DriverService()
 
@@ -8,6 +8,15 @@ driver_service = DriverService()
 def get_driver_profile(user_id: str):
     try:
         return driver_service.get_driver_profile(user_id)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+
+@driver_router.put("/profile/{user_id}", response_model=DriverProfileOut)
+def update_profile(user_id: str, updates: DriverProfileUpdate):
+    try:
+        return driver_service.update_driver_profile(user_id, updates)
     except HTTPException as e:
         raise e
     except Exception as e:
